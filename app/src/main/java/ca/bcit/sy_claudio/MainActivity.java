df6fa4f9500ba67aa30fc1312536c943bfb09855
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2020-10-17 15:51:34 -0700

    Created first screen.

--- app/src/main/java/ca/bcit/sy_claudio/MainActivity.java
+++ app/src/main/java/ca/bcit/sy_claudio/MainActivity.java
@@ -3,12 +3,26 @@ package ca.bcit.sy_claudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.inputtext);
+    }
+
+    public void search(View view) {
+        String toSearch = input.getText().toString();
+        if (TextUtils.isEmpty(toSearch)) {
+            input.setError("Cannot leave as blank.");
+        }
     }
 }
