93d6e1c75568d42617ba880cf724eb31a36e8991
Kent Claudio <rikysmile33@gmail.com>
2020-10-17 22:41:52 -0700

    Created third screen, ResultDataActivity

---
+++ app/src/main/java/ca/bcit/sy_claudio/ResultDataActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.sy_claudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_data);
+    }
+}
