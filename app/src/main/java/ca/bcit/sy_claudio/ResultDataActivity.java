5b3bf4ccdc0ec2a9abfaeab46b87d778cece5a34
Kent Claudio <rikysmile33@gmail.com>
2020-10-18 17:59:08 -0700

    Revised ResultDataActivity code and layout design to include source name and relevant comments

--- app/src/main/java/ca/bcit/sy_claudio/ResultDataActivity.java
+++ app/src/main/java/ca/bcit/sy_claudio/ResultDataActivity.java
@@ -16,7 +16,7 @@ public class ResultDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result_data);
         getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#595359\">" + getString(R.string.app_name) + "</font>"));
 
-
+        // get intent and store the json string values passed in
         Intent intent = getIntent();
         String source = intent.getStringExtra("source");
         String author = intent.getStringExtra("author");
@@ -27,21 +27,27 @@ public class ResultDataActivity extends AppCompatActivity {
         String publishedate = intent.getStringExtra("publishedat");
         String content = intent.getStringExtra("content");
 
+        // instantiate TextView objects and link them to respective layout elements
         TextView tvSource = findViewById(R.id.source);
         TextView tvAuthor = findViewById(R.id.author);
         TextView tvTitle = findViewById(R.id.title);
         TextView tvDescription = findViewById(R.id.description);
         TextView tvUrl = findViewById(R.id.url);
         ImageView imageThumbnail = findViewById(R.id.smallThumbnail);
+        // if thumbnail is not null
         if (urltoimage != null) {
+            // download and set that image to ImageView
             new ImageDownloaderTask(imageThumbnail).execute(urltoimage);
         }
         TextView tvPublishedAt = findViewById(R.id.publishedAt);
         TextView tvContent = findViewById(R.id.content);
 
+        // set text for respective layout elements
         tvAuthor.setText("by " + author);
+        // emojis are appended and prepended to the title
         tvTitle.setText("\uD83D\uDD25\uD83D\uDCAF\uD83D\uDE2E " + title +" \uD83D\uDE2E\uD83D\uDCAF\uD83D\uDD25");
         tvDescription.setText(description);
+        tvSource.setText("Source: " + source);
         tvUrl.setText(url);
         tvPublishedAt.setText("Published at " + publishedate);
         tvContent.setText(content);
